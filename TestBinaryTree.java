945bc2ab9f5d5e724b515f51a5bd1aced61b62d2
TanmayK24 <72218538+TanmayK24@users.noreply.github.com>
2020-11-10 17:06:17 +0000

    Test Cases for DAG added and previous cases modified

--- TestBinaryTree.java
+++ TestBinaryTree.java
@@ -13,72 +13,109 @@ public class TestBinaryTree {
 		assertTrue(tree.root != null);
 		//fail("Not yet implemented");
 	}
-	
+
 	@Test
-	public void testBranch() {
-		BinarySearchTree tree = new BinarySearchTree();
-		tree.root = new Node(5);
-		assertTrue(tree.root.left == null);
-		assertTrue(tree.root.right == null);
-		tree.root.left = new Node(4);
-		assertTrue(tree.root.left != null);
-		tree.root.right = new Node(6);
-		assertTrue(tree.root.right != null);
-		//fail("Not yet implemented");
+	public void testBinaryTree() {
+						/*
+				        0
+				       / \
+				      1   3
+				     / \
+				    7   4
+				         \
+				          5
+						 */
+		
+		Node[] nodes = new Node[8];
+		for (int i = 0; i < nodes.length; i++) nodes[i] = new Node(i);
+		Node root0 = nodes[0];
+		root0.children = new Node[] {nodes[1], nodes[3]};
+		nodes[1].children = new Node[] {nodes[7], nodes[4]};
+		nodes[4].children = new Node[] {nodes[5]};
+
+		BinarySearchTree g1 = new BinarySearchTree(root0);
+		assertEquals("LCA of 0 and 5 is", "0", g1.findLCA(nodes[0], nodes[5]).toString());
+		assertEquals("LCA of 7 and 4 is", "1", g1.findLCA(nodes[7], nodes[4]).toString());
+		assertEquals("LCA of 1 and 3 is", "0", g1.findLCA(nodes[1], nodes[3]).toString());
+		assertEquals("LCA of 5 and 7 is", "1", g1.findLCA(nodes[5], nodes[7]).toString());
+		assertEquals("LCA of 3 and 4 is", "0", g1.findLCA(nodes[3], nodes[4]).toString());
 	}
+
+	@Test
+	public void testTreeMultipleParents() {
+
+		/*
+
+			 		 1					 1
+				   / |  \ 
+				  2  |    3
+				  \  |  / |
+				   \ | /  |
+				     4	  |
+				     \	  |
+				      \	  |
+				       \  |
+				      	  5
+
+		 */
+
+		Node[] nodes4 = new Node[6];
+		for (int i = 0; i < nodes4.length; i++) nodes4[i] = new Node(i);
+		Node root3 = nodes4[1];
+		root3.children = new Node[] {nodes4[2], nodes4[4], nodes4[3], nodes4[5]};
+		nodes4[2].children = new Node[] {nodes4[4]};
+		nodes4[3].children = new Node[] {nodes4[4], nodes4[5]};
+		nodes4[4].children = new Node[] {nodes4[5]};
+		nodes4[5].children = null;
+
+		BinarySearchTree g1 = new BinarySearchTree(root3);
+		assertEquals("LCA of 1 and 3 is", "1", g1.findLCA(nodes4[1], nodes4[3]).toString());
+		assertEquals("LCA of 3 and 3 is", "3", g1.findLCA(nodes4[3], nodes4[3]).toString());
+		assertEquals("LCA of 2 and 3 is", "1", g1.findLCA(nodes4[2], nodes4[3]).toString());
+	}
+	
 	
 	@Test
 	public void LCA() {
-		BinarySearchTree tree = new BinarySearchTree();
-    	tree.root = new Node(5);
-    	tree.root.left = new Node(4);
-    	tree.root.left.right = new Node(3);
-    	tree.root.right = new Node(6);
-    	tree.root.right.right = new Node(7);
-    	tree.root.left.left = new Node(2);
-    	//print2D(tree.root);
-    	/*
-    	 
-               7     
-			 / 
-           6  
-		  /	
-		5
-		  \		3
-           4 /      
-			 \
-   				2
+		/*
+   	 
+            7     
+		  / 
+        6  
+	  /	
+	5
+	  \		3
+    	4 /      
+		  \
+			2
 
-               
-		LCA of 3 and 7 is : 5
-		LCA of 2 and 4 is : 4
-		LCA of 6 and 3 is : 5
-		LCA of 2 and 3 is : 4
-		LCA of 5 and 6 is : 5
-		LCA of 5 and 5 is : 5
-		LCA of 7 and 7 is : 7
+        
+	LCA of 3 and 7 is : 5
+	LCA of 2 and 4 is : 4
+	LCA of 6 and 3 is : 5
+	LCA of 2 and 3 is : 4
+	LCA of 5 and 6 is : 5
+	LCA of 5 and 5 is : 5
+	LCA of 7 and 7 is : 7
 
-    	 */
-    	assertEquals("LCA of 3 and 7 is : ",5, tree.findLCA(3,7));
-    	assertEquals("LCA of 2 and 4 is : ",4, tree.findLCA(2,4));
-    	assertEquals("LCA of 6 and 3 is : ",5, tree.findLCA(6,3));
-    	assertEquals("LCA of 2 and 3 is : ",4, tree.findLCA(2,3));
-    	assertEquals("LCA of 5 and 6 is : ",5, tree.findLCA(5,6));
-    	assertEquals("LCA of 5 and 5 is : ",5, tree.findLCA(5,5));
-    	assertEquals("LCA of 7 and 7 is : ",7, tree.findLCA(7,7));
-    	assertEquals("LCA of 4 and null entry is : ",-1, tree.findLCA(7,11));
+	 */
+		Node[] nodes = new Node[8];
+		for (int i = 0; i < nodes.length; i++) nodes[i] = new Node(i);
+		Node root0 = nodes[5];
+		root0.children = new Node[] {nodes[4], nodes[6]};
+		nodes[4].children = new Node[] {nodes[2], nodes[3]};
+		nodes[6].children = new Node[] {nodes[7]};
 
+		BinarySearchTree g1 = new BinarySearchTree(root0);
+		assertEquals("LCA of 3 and 7 is", "5", g1.findLCA(nodes[3], nodes[7]).toString());
+		assertEquals("LCA of 2 and 4 is", "4", g1.findLCA(nodes[2], nodes[4]).toString());
+		assertEquals("LCA of 6 and 3 is", "5", g1.findLCA(nodes[6], nodes[3]).toString());
+		assertEquals("LCA of 2 and 3 is", "4", g1.findLCA(nodes[2], nodes[3]).toString());
+		assertEquals("LCA of 5 and 6 is", "5", g1.findLCA(nodes[5], nodes[6]).toString());
+		assertEquals("LCA of 5 and 5 is", "5", g1.findLCA(nodes[5], nodes[5]).toString());
+		assertEquals("LCA of 7 and 7 is", "7", g1.findLCA(nodes[7], nodes[7]).toString());
 		
-		assertEquals("LCA of 3 and 7 is : ",5, tree.findLCA(3,7));
-    	assertEquals("LCA of 2 and 4 is : ",4, tree.findLCA(2,4));
-    	assertEquals("LCA of 6 and 3 is : ",5, tree.findLCA(6,3));
-    	assertEquals("LCA of 2 and 3 is : ",4, tree.findLCA(2,3));
-    	assertEquals("LCA of 5 and 6 is : ",5, tree.findLCA(5,6));
-    	assertEquals("LCA of 5 and 5 is : ",5, tree.findLCA(5,5));
-    	assertEquals("LCA of 7 and 7 is : ",7, tree.findLCA(7,7));
-    	assertEquals("LCA of 4 and null entry is : ",-1, tree.findLCA(7,11));
-
-		//fail("Not yet implemented");
+		
+		
 	}
-	
-}
+}	
